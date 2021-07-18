143aef80136e9d00650c96a2687c2aa2ba2c3ab1
felipe <pedrofelipeortiz@gmail.com>
2021-07-18 10:43:53 -0500

    primeros avances

---
+++ src/components/Account.jsx
@@ -0,0 +1,14 @@
+import  Navbar  from "./Navbar";
+
+const Account = () => {
+
+    return ( 
+        
+        <div>
+            <Navbar/>
+            hello acount
+        </div>
+     );
+}
+ 
+export default Account;
---
+++ src/components/CuidaTuCanino.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+const CuidaTuCanino = () => {
+    return ( <div>
+        <Navbar/>
+        CuidaTuCanino
+    </div> );
+}
+ 
+export default CuidaTuCanino;
